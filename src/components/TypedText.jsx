4bc8dec3cca53cfbb09f9b091ae71a491acf3297
Mack Talcott <mack@tal.co.tt>
2023-07-02 23:23:16 -0700

    Basic content and typed text name

---
+++ src/components/TypedText.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Typed from 'typed.js';
+
+export function TypedText({text}) {
+	// Create reference to store the DOM element containing the animation
+	const el = React.useRef(null);
+	React.useEffect(() => {
+		const typed = new Typed(el.current, {
+		  strings: [text],
+		  typeSpeed: 50,
+		  startDelay: 600
+		});
+	
+		return () => {
+		  // Destroy Typed instance during cleanup to stop animation
+		  typed.destroy();
+		};
+	  }, []);
+	
+	  return <span className="typed-text" ref={el} />;
+}
+
+<style>
+	.typed-text {
+		
+	}
+</style>
