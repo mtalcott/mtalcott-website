2ff260b35e7305ea391a8acabe50c9ca5cb9d49c
Mack Talcott <mack@tal.co.tt>
2023-07-04 15:32:11 -0700

    Prettier formatting

--- src/components/TypedTextPrompt.jsx
+++ src/components/TypedTextPrompt.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import Typed from 'typed.js';
+import Typed from "typed.js";
 
-export function TypedTextPrompt({text}) {
-	// Create reference to store the DOM element containing the animation
-	const el = React.useRef(null);
-	React.useEffect(() => {
-		const typed = new Typed(el.current, {
-		  strings: [text],
-		  typeSpeed: 50,
-		  startDelay: 600
-		});
-	
-		return () => {
-		  // Destroy Typed instance during cleanup to stop animation
-		  typed.destroy();
-		};
-	  }, []);
-	
-	  return (
-		<span className="accent-text-gradient">
-			<span>$ </span>
-			<span className="inline-block min-w-[21px]">
-				<span className="typed-text" ref={el} />
-			</span>
-		</span>
-	  );
+export function TypedTextPrompt({ text }) {
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [text],
+      typeSpeed: 50,
+      startDelay: 600,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <span className="accent-text-gradient">
+      <span>$ </span>
+      <span className="inline-block min-w-[21px]">
+        <span className="typed-text" ref={el} />
+      </span>
+    </span>
+  );
 }
