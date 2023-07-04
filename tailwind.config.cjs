f9a7953cdaac9d2f11e4203876160c1f292d1913
Mack Talcott <mack@tal.co.tt>
2023-07-04 14:31:01 -0700

    Add accent colors to Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				accent: "#7c3aed",
+				accent2: "#da62c4"
+			}
+		},
 	},
 	plugins: [],
 }
